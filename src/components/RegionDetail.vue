4fc87f6a0e1e5a20a94ef29385683feb02ba7cb5
Harley Bramwell <harleybramwell97@gmail.com>
2020-02-23 22:13:55 +0000

    first attempt

---
+++ src/components/RegionDetail.vue
@@ -0,0 +1,23 @@
+  <template lang="html">
+    <div v-if="region">
+      <h2>{{region.shortname}}</h2>
+      <p>{{region.intensity.forecast}}</p>
+      <ul v-for="(title, index) in region.generationmix[0]" :object='object' :key='index'>
+        <li>{{object.fuel}}{{object.perc}}</li>
+
+
+      </ul>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'region-detail',
+  props: ['region']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/RegionList.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+<div>
+  <h3>Select a region:</h3>
+  <ul>
+    <region-item v-for="(region, index) in regionListArray.data.regions[0]" :region='region' :key="index"/>
+  </ul>
+
+</div>
+</template>
+
+<script>
+import RegionItem from './RegionItem.vue'
+export default {
+  name: 'region-list',
+  props: ["regionListArray"],
+  components: {
+    'region-item': RegionItem
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
